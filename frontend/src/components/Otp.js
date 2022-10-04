d0a5d2fd034362a24429b635a0267a4a72758764
be-u-nik <nikhil2001rockz@gmail.com>
2022-10-04 10:49:35 +0530

    backend url updated

--- frontend/src/components/Otp.js
+++ frontend/src/components/Otp.js
@@ -29,9 +29,9 @@ function Otp() {
     // }
     await axios
       .post(
-        `http://localhost:8000/api/users/otp/${localStorage.getItem(
-          "otpToken"
-        )}`,
+        `${
+          process.env.REACT_APP_BACKEND_URL
+        }/api/users/otp/${localStorage.getItem("otpToken")}`,
         formData
       )
       .then((res) => {
